f76e0f30d3cc1dc473771807bafcd772e47833e9
om2009 <80381254+om2009@users.noreply.github.com>
2021-09-21 19:36:55 +0530

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,15 +1,13 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDn8jRO-XinofBYChF8YYUtyWfylBUehas",
-    authDomain: "project-eb44e.firebaseapp.com",
-    databaseURL: "https://project-eb44e-default-rtdb.firebaseio.com",
-    projectId: "project-eb44e",
-    storageBucket: "project-eb44e.appspot.com",
-    messagingSenderId: "274272917166",
-    appId: "1:274272917166:web:261a00fd28680a0156bf3f"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+    apiKey: "AIzaSyCys5K_BxIn0Mhk6yKH7rGrDPrwI6OVizw",
+    authDomain: "project-4447c.firebaseapp.com",
+    databaseURL: "https://project-4447c-default-rtdb.firebaseio.com",
+    projectId: "project-4447c",
+    storageBucket: "project-4447c.appspot.com",
+    messagingSenderId: "188654649048",
+    appId: "1:188654649048:web:79c0a726044093393aa1ad"
+  }
+  firebase.initializeApp(firebaseConfig);  
 
 user_name = localStorage.getItem("user_name");
 user_ad = localStorage.getItem("user_ad");
@@ -19,23 +17,40 @@ document.getElementById("user_name").innerHTML = "Welcome " + user_name;
 
 function buy_peppy_paneer()
 {
-firebase.database().ref("/").child("order").update({
+firebase.database().ref("/").child(user_name + user_ad).update({
     pizza : "Peppy Paneer",
     user_name : user_name,
     user_ad : user_ad
 });
 localStorage.setItem("pizza", "Peppy Paneer");
-window.location = "thanks.html";
+setTimeout(function()
+    {
+        window.location = "thanks.html";
+    },2000);
 }
 
 
 function buy_cheese_n_corn()
 {
-    firebase.database().ref("/").child("order").update({
+    firebase.database().ref("/").child(user_name + user_ad).update({
         pizza : "CHEESE N CORN",
         user_name : user_name,
         user_ad : user_ad
     });
     localStorage.setItem("pizza", "CHEESE N CORN");
-    window.location = "thanks.html";
+    setTimeout(function()
+    {
+        window.location = "thanks.html";
+    },2000);
+}
+
+function logout()
+{
+    localStorage.removeItem("user_name");
+    localStorage.removeItem("user_ad");
+    window.location = "index.html";
+}
+function orders()
+{
+    window.location = "orders.html";
 }
