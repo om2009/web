63d6009dbc09426d6b3f7b860012ce99ed4df2cb
om2009 <80381254+om2009@users.noreply.github.com>
2021-10-14 13:07:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
-function startClassification()
+prediction = "";
+
+
+
+Webcam.set({
+    width : 350,
+    height : 300,
+    image_format : 'png',
+    png_qulity : 90
+});
+
+camera = document.getElementById("camera");
+
+
+Webcam.attach('#camera');
+
+
+function take_snapshot()
 {
-    navigator.mediaDevices.getUserMedia({audio: true});
-    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/aV13GYJB8/model.json', modelReady);
-
+    Webcam.snap(function(data_uri)
+    {
+        document.getElementById("result").innerHTML = '<img id ="capture_image" src ="'+data_uri+'"/>';
+    });
 }
- function modelReady()
- {
-    classifier.classify(gotResults);
- }
 
- function gotResults(error, results){
+console.log('ml5 version : ', ml5.version);
 
-    console.log(results);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/uOQ3gbWX8/model.json", modelLoaded); 
+
+function modelLoaded()
+{
+    console.log('Model ready');
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+    synth.speak(utterThis);
 }
