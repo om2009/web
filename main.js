9bc4815c25bd0c229a9263f8fede5b8209327b92
om2009 <80381254+om2009@users.noreply.github.com>
2021-10-22 20:40:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,50 +1,18 @@
-function preload()
-{
-}
+function preload(){
 
-function setup()
-{
-    canvas = createCanvas(500,400);
-    canvas.position(110,250);
-    video = createCapture(VIDEO);
-    video.hide();
-
-    frame_color = "gray"
 }
 
 
+function setup(){
+    canvas = createCanvas(400, 350);
+    canvas.center();
+}
 
 function draw()
 {
-    image(video, 70, 50, 350, 300);
 
-    let c;
-    noStroke();
-    c = color(frame_color);
-    fill(c);
-    rect(30, 10, 435, 35); 
-
-    c = color(frame_color);
-    fill(c);
-    rect(30, 40, 35, 350); 
-
-    c = color(frame_color);
-    fill(c);
-    rect(430, 40, 35, 350);
-
-    c = color(frame_color);
-    fill(c);
-    rect(30, 355, 435, 35);
 }
 
-
-function take_snapshot()
-{
-    save('filter image.png');
-}
-
-
-function framecolor()
-{
-    frame_color = document.getElementById("color_name").value;
+function take_snapshot(){
+    save('myFilterImage.png');
 }
