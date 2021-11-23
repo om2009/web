73cc8ff22919de5793c56112e28bae1d8c687dec
om2009 <80381254+om2009@users.noreply.github.com>
2021-11-23 13:05:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,33 @@
 function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    video.position(50,150);
+
     canvas = createCanvas(500, 500)
-    canvas.position(550,150);
+    canvas.position(750,150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPose);
+}
+
+
+
+function draw()
+{
+    background(" #9494b8");
+}
+
+
+function modelLoaded()
+{
+    console.log("posenet is ready");
+}
+
+
+
+function gotPose(result)
+{
+    if(result.length > 0)
+    {
+        console.log(result);
+    }
 }
