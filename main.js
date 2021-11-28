63fe3c7ba6a19425fcf69f9d91b56d97fa299198
om2009 <80381254+om2009@users.noreply.github.com>
2021-11-28 19:10:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,5 @@
-function setup(){
-    video = createCapture(VIDEO);
-    video.size(550, 500);
-    video.position(50,150);
-
-    canvas = createCanvas(500, 500)
-    canvas.position(750,150);
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPose);
-}
-
-
-function draw()
+function setup()
 {
-    user_name = document.getElementById("user_name").value;
-    background(" #9494b8");
-    textSize(difference);
-    fill('white');
-    text(user_name, 50,400)
-}
-
-
-function modelLoaded()
-{
-    console.log("posenet is ready");
-}
-left_wrist_x =0;
-right_wrist_x = 0;
-difference =0;
-function gotPose(result)
-{
-    if(result.length > 0)
-    {
-        console.log(result);
-
-       left_wrist_x = result[0].pose.leftWrist.x;
-       right_wrist_x = result[0].pose.rightWrist.x;
-       console.log("left wrist : " +left_wrist_x +"right wrist : " +right_wrist_x)
-       difference = floor(left_wrist_x-right_wrist_x);
-       console.log(difference + " difference");
-
-    }
+    canvas = createCanvas(600, 450);
+    canvas.center();
 }
