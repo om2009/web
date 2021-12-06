bfba0a12e00ab0396d052560f2c0a74a1da44d03
om2009 <80381254+om2009@users.noreply.github.com>
2021-12-06 15:27:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 song_1 = "";
 song_2 = "";
+
+score_left_wrist = 0;
+
 left_wrist_x = 0;
 
 left_wrist_y = 0;
@@ -7,6 +10,10 @@ left_wrist_y = 0;
 right_wrist_x = 0;
 
 right_wrist_y = 0;
+
+//song1_status = song_1.isPlaying();
+
+
 function preload()
 {
     song_1 = loadSound("music.mp3");
@@ -32,6 +39,7 @@ function gotPoses(result)
     if(result.length > 0)
     {
     console.log(result);
+    score_left_wrist = result[0].pose.keypoints[9].score;
     left_wrist_x = result[0].pose.leftWrist.x;
     left_wrist_y = result[0].pose.leftWrist.y;
     right_wrist_x = result[0].pose.rightWrist.x;
@@ -44,6 +52,18 @@ function gotPoses(result)
 function draw()
 {
     image(video,0, 0, 600, 450);
+    fill('#FF0000');
+    stroke('#FF0000');
+    if(score_left_wrist >0.2)
+    {
+        circle(left_wrist_x, left_wrist_y, 20);
+        song_2.stop();
+        if(song_1.isPlaying()== false)
+        {
+            song_1.play();
+            document.getElementById("song_name").innerHTML = "harry potter";
+        }
+    }
 }
 
 function play()
